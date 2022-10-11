6a3f934b717776a9e7b4453b02cbf5ee7fce6178
Mark E. Haase <mhaase@mitre.org>
2022-10-11 13:01:05 -0400

    Add a warning if the user tries to run builder on a mobile (touchscreen) device

--- src/attack_flow_builder/src/main.ts
+++ src/attack_flow_builder/src/main.ts
@@ -3,4 +3,13 @@ import store from './store'
 import { createApp } from 'vue'
 import "@/assets/fonts/inter.css"
 
+function isTouchEnabled() {
+    return ('ontouchstart' in window) ||
+        (navigator.maxTouchPoints > 0);
+}
+
+if (isTouchEnabled()) {
+    alert("This application does not support touch screens.");
+}
+
 createApp(App).use(store).mount('#app')
