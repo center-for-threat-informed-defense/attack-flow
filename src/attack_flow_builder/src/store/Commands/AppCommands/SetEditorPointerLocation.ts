6c05ffa7d863018e6655957cc51c927dceb5aa6d
Michael Carenzo <mcarenzo@mitre.org>
2022-10-25 11:54:05 -0400

    Added submenu create and bug fixes

---
+++ src/attack_flow_builder/src/store/Commands/AppCommands/SetEditorPointerLocation.ts
@@ -0,0 +1,46 @@
+import { AppCommand } from "../AppCommand";
+import { ApplicationStore } from "@/store/StoreTypes";
+import { EditorViewParameters, PageEditor } from "@/store/PageEditor";
+
+// export class SetEditorPointerLocation extends AppCommand {
+
+//     // /**
+//     //  * The editor.
+//     //  */
+//     // private _editor: PageEditor;
+
+//     // /**
+//     //  * The new front-end view parameters.
+//     //  */
+//     // private _params: EditorViewParameters;
+
+
+//     // /**
+//     //  * Sets a page editor's front-end view parameters.
+//     //  * @param context
+//     //  *  The application context.
+//     //  * @param id
+//     //  *  The id of the page.
+//     //  * @param params
+//     //  *  The new front-end view parameters.
+//     //  */
+//     // constructor(context: ApplicationStore, id: string, params: EditorViewParameters) {
+//     //     super(context);
+//     //     let editor = context.pages.get(id);
+//     //     if(!editor) {
+//     //         throw new Error(`Page '${ id }' not found.`);
+//     //     } else {
+//     //         this._editor = editor;
+//     //     }
+//     //     this._params = params;
+//     // }
+
+
+//     // /**
+//     //  * Executes the command.
+//     //  */
+//     // public execute(): void {
+//     //     this._editor.view = this._params;
+//     // }
+
+// }
