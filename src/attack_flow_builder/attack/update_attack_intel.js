d4d63c2ccc58534e46aad7c3955010eb5d9fa5fc
Michael Carenzo <79934822+mikecarenzo@users.noreply.github.com>
2023-08-03 13:42:04 -0400

    create intel file

---
+++ src/attack_flow_builder/attack/update_attack_intel.js
@@ -0,0 +1,70 @@
+const { resolve } = require("path");
+const { writeFileSync } = require("fs");
+const { fetchAttackData } = require("./download_attack");
+
+/**
+ * The base URL for the ATT&CK repository.
+ */
+const BASE_URL = "https://raw.githubusercontent.com/mitre-attack/attack-stix-data/master";
+
+/**
+ * The intel file's export key.
+ */
+const EXPORT_KEY = "intel";
+
+/**
+ * The intel file's path.
+ */
+const INTEL_FILE_PATH = "../src/assets/builder.config.intel.ts";
+
+/**
+ * JavaScript variable regex.
+ */
+const JS_VAR_REGEX = /^[a-z_$][a-z0-9_$]*$/i;
+
+/**
+ * Updates the specified intel file.
+ * @param {string} path
+ *  The intel file's path.
+ * @param  {...string} urls
+ *  A list of STIX manifests specified by url. 
+ */
+async function updateApplicationAttackIntel(path, ...urls) {
+    path = resolve(__dirname, path);
+    
+    // Validate export key
+    if(!JS_VAR_REGEX.test(EXPORT_KEY)) {
+        throw new Error(`Export key '${ EXPORT_KEY }' is not a valid variable name.`);
+    }
+    
+    // Collect intel
+    let types = await fetchAttackData(...urls);
+    console.log("→ Generating Application Intel File...");
+    let intel = {
+        tactics        : types.get("tactic"),
+        tactic_recs    : types.get("tactic").map(o => `${o.id} - ${o.name}`).sort(),
+        technique      : types.get("technique"),
+        technique_recs : types.get("technique").map(o => `${o.id} - ${o.name}`).sort()
+    };
+    
+    // Generate intel file
+    let file = "";
+    file += `export const ${ EXPORT_KEY } = `;
+    file += JSON.stringify(intel, null, 4);
+    file += `;\n\nexport default ${ EXPORT_KEY };\n`
+    writeFileSync(path, file);
+
+    // Done
+    console.log("\nIntelligence updated successfully.\n");
+
+}
+
+/**
+ * Main
+ */
+updateApplicationAttackIntel(
+    INTEL_FILE_PATH,
+    `${BASE_URL}/enterprise-attack/enterprise-attack-13.0.json`,
+    `${BASE_URL}/ics-attack/ics-attack-13.0.json`,
+    `${BASE_URL}/mobile-attack/mobile-attack-13.0.json`
+);
