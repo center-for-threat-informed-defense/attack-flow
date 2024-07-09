6ad0acef4586a9de598bbcc261ac606129dab3b9
Mark E. Haase <mhaase@mitre.org>
2024-07-09 16:03:56 -0400

    Bump version v2.2.6 -> v2.2.7

--- docs/conf.py
+++ docs/conf.py
@@ -22,7 +22,7 @@ copyright = "2022, Center for Threat-Informed Defense"
 author = "Center for Threat-Informed Defense"
 
 # The full version, including alpha/beta/rc tags
-version = "v2.2.6"
+version = "v2.2.7"
 release = version
 
 
