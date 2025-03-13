9e55e40548dc253c639ca09cc3615f17cf9d9e96
arobbins <arobbins@mitre.org>
2025-03-13 09:45:02 -0400

    update js and css for sphinx theme

--- docs/_static/js/ctid.js
+++ docs/_static/js/ctid.js
@@ -18,27 +18,35 @@ window.addEventListener("scroll", navHighlighter);
 
 function navHighlighter() {
     // Get current scroll position
-    let scrollY = window.scrollY;
+    const targetY = + document.querySelector("header").offsetHeight;
+    let closestEl = null;
+    let closestDist = 999999;
+    const navLinks = document.querySelectorAll(".page-toc li a");
 
-    headings.forEach((current) => {
-        const headingHeight = current.offsetHeight;
-        const headingTop = current.offsetTop;
-        const headingId = current.innerText;
-        // look for the heading that matches current scroll position
-        if (
-            headingId &&
-            scrollY > headingTop &&
-            scrollY <= headingTop + headingHeight
-        ) {
-            // search nav items for the heading that matches
-            const navItems = document.querySelectorAll(".page-toc li a");
-            navItems.forEach((navItemMatch) => {
-                if (navItemMatch.innerText === headingId) {
-                    navItemMatch.classList.add("active");
-                } else {
-                    navItemMatch.classList.remove("active");
-                }
-            });
+    for (const heading of headings) {
+        const rect = heading.getBoundingClientRect();
+        if (rect.top > window.innerHeight) {
+            // This element is off screen;
+            continue;
         }
-    });
+
+        // Look for the heading who's midpoint is closest to the target coordinate.
+        const yMidpoint = rect.top + rect.height / 2;
+        const dist = yMidpoint - targetY;
+        if (dist > 0 && dist < closestDist) {
+            closestDist = dist;
+            closestEl = heading;
+        }
+    }
+    // If you found a heading and it matches a navlink, then make that navlink the
+    // current active link.
+    if (closestEl) {
+        for (const navLink of navLinks) {
+            if (navLink.innerText.trim() === closestEl.innerText.trim()) {
+                navLinks.forEach(nl => nl.classList.remove("active"));
+                navLink.classList.add("active");
+                break;
+            }
+        }
+    }
 }
